a1430c38135a78efcb036459a855368a63a64ea9
L-for-Lanciz <74201903+L-for-Lanciz@users.noreply.github.com>
2021-01-21 18:56:16 +0100

    Reduced gas fee for some functions

--- RestAPI/blockchain/contracts/Rebis.sol
+++ RestAPI/blockchain/contracts/Rebis.sol
@@ -31,32 +31,6 @@ contract Rebis {
     states rentalState;
   }
 
-  event RentalCreated(
-    uint id,
-    address payable renter,
-    address payable customer,
-    uint fee,
-    uint deposit,
-    states rentalState
-  );
-
-  event BikeRented(
-    uint id,
-    address payable renter,
-    address customer,
-    uint fee,
-    uint deposit,
-    states rentalState
-  );
-
-  event RentalEnded(
-    uint id,
-    address renter,
-    address payable customer,
-    uint deposit,
-    states rentalState
-  );
-
   function createRental(uint _fee, uint _deposit) public {
     //set a condition, can't have free rentals
     require (_fee > 0);
@@ -64,45 +38,37 @@ contract Rebis {
     rentalCounter++;
     //create a new rental
     rentals[rentalCounter] = Rental(rentalCounter, msg.sender, address(0), _fee, _deposit, states.PENDING);
-    //trigger event
-    emit RentalCreated(rentalCounter, msg.sender, address(0), _fee, _deposit, states.PENDING);
   }
 
   function rentingCustomer(uint _id) public payable {
     //check the mutex for reentrancy
-    require(!reEntrancyMutex1);
+    require(!reEntrancyMutex1, "Mutex must be false");
     //target rental must exist
-    require(_id> 0 && _id<= rentalCounter);
-    //get the target rental
-    Rental memory _rental = rentals[_id];
-    require(_rental.customer != msg.sender);
+    require(_id> 0 && _id<= rentalCounter, "RentalID is out of bound");
+    require(rentals[_id].customer != msg.sender, "Customer equals the sender");
     //now we can instantiate the customer
     rentals[_id].customer = msg.sender;
     //customer must have enough money
-    uint totalAmount = _rental.fee + _rental.deposit;
-    require (msg.sender.balance >= totalAmount);
-    require(_rental.rentalState == states.PENDING);
+    uint totalAmount = rentals[_id].fee + rentals[_id].deposit;
+    require (msg.sender.balance >= totalAmount, "Not enough ETH in balance");
+    require(rentals[_id].rentalState == states.PENDING, "State is not correct");
     //avoid re-entrancy
     reEntrancyMutex1 = true;
     //transfer eth
-    _rental.renter.transfer(msg.value);
+    rentals[_id].renter.transfer(msg.value);
     rentals[_id].rentalState = states.RENTED;
     //release the reEntrancyMutex1
     reEntrancyMutex1 = false;
-    //trigger event
-    emit BikeRented(_id, _rental.renter, msg.sender, _rental.fee, _rental.deposit, states.RENTED);
   }
 
   function depositWarranty(uint _id) public payable {
     //target rental must exist
     require(_id> 0 && _id<= rentalCounter);
-    //get the target rental
-    Rental memory _rental = rentals[_id];
     //transfer eth
-    if (msg.value == 0 && _rental.deposit != 0) {
+    if (msg.value == 0 && rentals[_id].deposit != 0) {
       rentals[_id].deposit = 0;
     } else {
-      _rental.renter.transfer(msg.value);
+      rentals[_id].renter.transfer(msg.value);
     }
   }
 
@@ -127,8 +93,6 @@ contract Rebis {
     rentals[_id].rentalState = states.ENDED;
     //release the reEntrancyMutex2
     reEntrancyMutex2 = false;
-    //trigger event
-    emit RentalEnded(_id, msg.sender, _rental.customer, _rental.deposit, _rental.rentalState);
   }
 
   function rentalsDeployed() public view returns(uint) {
